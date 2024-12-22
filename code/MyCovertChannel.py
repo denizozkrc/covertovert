c688316abf8285dc149b59ccb519f6622d57056d
denizozkrc <ozkiric.deniz@metu.edu.tr>
2024-12-22 20:11:36 +0300

    code fixes

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -23,12 +23,17 @@ class MyCovertChannel(CovertChannelBase):
         payload = self.generate_random_message()
         chunks = [message[i:i+bit_chunk_size] for i in range(0, len(message), bit_chunk_size)]
         print("ch0: ", chunks[0], ", ch1: ", (chunks[1]))
-        print("message: ", message[0], ", type: ", (message[0]))
         for chunk in chunks:
+            
+            print("-------------------------------")
             # encode here
-            transaction_id = self.encode(chunk,bit_chunk_size)
+            transaction_id = self.encode(chunk, bit_chunk_size)
+            print("trid: ", transaction_id)
+            print("type of tr: ", transaction_id.bit_length())
             dns_request = IP(dst=receiver_IP)/UDP(dport=53)/DNS(id=transaction_id, qd=DNSQR(qname=payload))
+            print("hey")
             send(dns_request)
+            print("ho")
         print("Message sent covertly!")
 
     def receive(self, sender_IP, log_file_name, bit_chunk_size):
@@ -46,16 +51,16 @@ class MyCovertChannel(CovertChannelBase):
             return stop_sniffing
 
         def process_packet(packet):
+            print("-------------------------------")
             print("got a packet!!")
             nonlocal mod_var, message_char, message, stop_sniffing
             if packet.haslayer(DNS):
                 mod_var += 1
                 print(type(packet[DNS].id))
                 transaction_id = packet[DNS].id
-                print("got id")
-                chunk = transaction_id.to_bytes(2, byteorder='big')
-                print("chunk type: ", type(chunk), ", chunk: ", chunk)
-                # decode here
+                print("will start decoding")
+                chunk = self.decode(transaction_id, bit_chunk_size)
+                print("done decoding")
                 if message_char == "":
                     message_char = chunk
                 else:
@@ -63,15 +68,13 @@ class MyCovertChannel(CovertChannelBase):
                 if mod_var == 8 // bit_chunk_size:
                     mod_var = 0
                     print("message_char: ", message_char, ", message_char type: ", type(message_char))
-                    message_char_to_binary_str = ''.join(format(byte, '08b') for byte in message_char)
-                    print("here")
-                    temp = self.convert_eight_bits_to_character(message_char_to_binary_str[-4:])
-                    message.append(temp)
-                    message_char = ""
-                    if temp == '.':
+                    message_char = self.convert_eight_bits_to_character(message_char)
+                    message.append((message_char))
+                    if message_char == '.':
                         stop_sniffing = True
                     else:
                         stop_sniffing = False
+                    message_char = ""
             print(f"Extracted data chunk: {chunk}")
 
         print("Listening for covert data...")
@@ -79,29 +82,57 @@ class MyCovertChannel(CovertChannelBase):
 
         self.log_message("", log_file_name)
 
-    def encode(self,bit_string,bit_chunk_size):
+    def encode(self, bit_string, bit_chunk_size):
         """
         Takes the message part we want to encode
         Returns final transaction id
         """
-        value_to_be_sent = int(bit_string,2)
-        
-        transactionID = self.generate_random_binary_message(16,16)  # byte-string
+        print("bit string we are sending: ", bit_string)
+        value_to_be_sent = int(bit_string, 2)
+        transactionID = self.generate_random_binary_message(2, 2)  # byte-string
         trans_chunks = [transactionID[i:i+bit_chunk_size] for i in range(0, len(transactionID), bit_chunk_size)]
-        trans_int = [int(bitstr,2) for bitstr in trans_chunks]
+        trans_int = [int(bitstr, 2) for bitstr in trans_chunks]
         checksum = 0
         for part in trans_int:
             checksum += part
         checksum = checksum % (2**bit_chunk_size)
 
-        if value_to_be_sent != checksum:
+        while value_to_be_sent != checksum:
+            print("fixing checksum, initial transaction id: ", transactionID)
             index = random.randint(0, 16//bit_chunk_size-1)
             for i in range(bit_chunk_size):
                 num = 2**i
                 if checksum & num != value_to_be_sent & num:
+                    print("flipped the bit")
                     trans_int[index] = trans_int[index] ^ num 
-            concatenated_binary = ''.join(trans_chunks)
-            transactionID = int(concatenated_binary, 2)
+                    break
+            trans_chunks[index] = format(trans_int[index], f'0{bit_chunk_size}b')
+            trans_int = [int(bitstr, 2) for bitstr in trans_chunks]
+            checksum = 0
+            for part in trans_int:
+                checksum += part
+            checksum = checksum % (2**bit_chunk_size)
+            transactionID = ''.join(trans_chunks)
+
+        print("sent transaction id: ", transactionID)
+        transactionID = int(transactionID, 2)
+        print("sent transaction id value : ", transactionID)
         return transactionID
 
-    
+    def decode(self, transactionID, bit_chunk_size):
+        """
+        Takes transactionID
+        Returns a binary string of length bit_chunk_size
+        """
+        binary_string_transactionID = format(transactionID, f'0{16}b')
+        print("received transID : ", binary_string_transactionID)
+        print("received transID value: ", transactionID)
+        trans_chunks = [binary_string_transactionID[i:i+bit_chunk_size] for i in range(0, len(binary_string_transactionID), bit_chunk_size)]
+        trans_int = [int(bitstr,2) for bitstr in trans_chunks]
+        checksum = 0
+        for part in trans_int:
+            checksum += part
+        checksum = checksum % (2**bit_chunk_size) # got the integer value of the bits
+        value = format(checksum, f'0{bit_chunk_size}b')
+        print("value received: ", value)
+        return value
