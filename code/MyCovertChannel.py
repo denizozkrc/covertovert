43364c5c3aa46c4af286aacab7cf69c6f27fca09
denizozkrc <ozkiric.deniz@metu.edu.tr>
2024-12-22 15:31:09 +0300

    Sender template implemented

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -1,4 +1,5 @@
 from CovertChannelBase import CovertChannelBase
+from scapy.all import IP, UDP, DNS, DNSQR, send
 
 class MyCovertChannel(CovertChannelBase):
     """
@@ -10,13 +11,20 @@ class MyCovertChannel(CovertChannelBase):
         - You can edit __init__.
         """
         pass
-    def send(self, log_file_name, parameter1, parameter2):
+    def send(self, log_file_name, min_msg_length, max_msg_length, bit_chunk_size, receiver_IP):
         """
         - In this function, you expected to create a random message (using function/s in CovertChannelBase), and send it to the receiver container. Entire sending operations should be handled in this function.
         - After the implementation, please rewrite this comment part to explain your code basically.
         """
-        binary_message = self.generate_random_binary_message_with_logging(log_file_name)
-        
+        message = self.generate_random_binary_message_with_logging(log_file_name, min_length=min_msg_length, max_length=max_msg_length)
+        payload = self.generate_random_message()
+        chunks = [message[i:i+bit_chunk_size] for i in range(len(message), bit_chunk_size)]
+        for chunk in chunks:
+            transaction_id = int.from_bytes(chunk.encode('utf-8'), byteorder='big')
+            dns_request = IP(dst=receiver_IP)/UDP(dport=53)/DNS(id=transaction_id, qd=DNSQR(qname=payload))
+            send(dns_request)
+        print("Message sent covertly!")
+
     def receive(self, parameter1, parameter2, parameter3, log_file_name):
         """
         - In this function, you are expected to receive and decode the transferred message. Because there are many types of covert channels, the receiver implementation depends on the chosen covert channel type, and you may not need to use the functions in CovertChannelBase.
